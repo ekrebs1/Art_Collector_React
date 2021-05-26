ce2e33a22770b5629f5d67388c97bfebda9d07fd
Erica <ericakrebs1@gmail.com>
2021-05-26 18:00:15 -0500

    part 2 finished

--- src/index.jsx
+++ src/index.jsx
@@ -6,32 +6,47 @@ import {
   //Feature,
   Loading,
   //Preview,
-  //Search,
+  Search,
   Title,
 } from "./components";
 
 const App = () => {
-  /**
-   * We are at the App level component, which is top-most. Any state which needs to be shared between immediate children should
-   * be made here, so create state pairs using useState() for:
-   *
-   * searchResults, setSearchResults (default should be this object:  {info: {}, records: []} )
-   * featuredResult, setFeaturedResult (default should be null)
-   * isLoading, setIsLoading (default should be false)
-   */
+  //SEARCH RESULTS
+  const [searchResults, setSearchResults] = useState({ info: {}, records: [] });
+
+  //Featured Result
+  const [featuredResult, setFeaturedResult] = useState(null);
+
+  //Loading
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <div className='app'>
-      {/* <Title /> is static, doesn't need any props */}
       <Title />
-      {/* <Search /> needs props for setIsLoading and setSearchResults (trigger <Loading /> on search start/end, and transfer results to preview) */}
-      {/* <Search /> */}
+      {/* <Search /> needs props for setIsLoading and setSearchResults (trigger <Loading /> on search start/end, and transfer results to preview)  */}
+
+      <Search setIsLoading={setIsLoading} setSearchResults={setSearchResults} />
+
       {/* <Preview /> needs props for searchResults, setIsLoading and setSearchResults (clicking prev/next buttons), and setFeaturedResult (clicking a preview) */}
-      {/* <Preview /> */}
+
+      {/* <Preview
+        searchResults={searchResults}
+        setIsLoading={setIsLoading}
+        setSearchResults={setSearchResults}
+        setFeaturedResult={setFeaturedResult}
+      /> */}
+
       {/* <Feature /> needs props for featuredResult, as well as setIsLoading and setSearchResults (clicking on searchable properties) */}
-      {/* <Feature /> */}
+
+      {/* <Feature
+        featuredResult={featuredResult}
+        setIsLoading={setIsLoading}
+        setSearchResults={setSearchResults}
+      /> */}
+
       {/* <Loading /> is static, but should only render when isLoading is true */}
-      <Loading /> {/* use a ternary and render null if isLoading is false */}
+      <Loading />
+      {/* use a ternary and render null if isLoading is false */}
     </div>
   );
 };
